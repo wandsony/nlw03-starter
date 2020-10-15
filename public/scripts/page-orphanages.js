214eae7ae79a032c704f1db25e8c89728fbade6b
Wandson Gomes <wandsony@gmail.com>
2020-10-14 22:22:13 -0300

    A escolha da stack

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,26 +1,29 @@
-// create map
+//create map
 const map = L.map('mapid').setView([-23.5344425, -46.6222736], 15);
 
-// create and add tileLayer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+//create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',)
+.addTo(map);
 
-// create icon
+
+//create icon
 const icon = L.icon({
     iconUrl: "./public/images/map-marker.svg",
     iconSize: [58, 68],
-    iconAnchor: [29, 68],
-    popupAnchor: [170, 2]
+    iconAnchor: [29,68],
+    popAnchor: [170, 2]
 })
 
-// create popo overlay
+// create popup overlay
 const popup = L.popup({
     closeButton: false,
-    className: 'map-popup',
+    className: "map-popup",
     minWidth: 240,
-    minHeigth: 240
-}).setContent('Lar das meninas <a herf="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
 
-// create and add marker
-L.marker([-23.5344425, -46.6222736], { icon })
-    .addTo(map)
-    .bindPopup(popup)
+//create and add marker
+L
+.marker([-23.5344425, -46.6222736], { icon })
+.addTo(map)
+.bindPopup(popup)
